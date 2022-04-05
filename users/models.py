fa8994cd6625053b4a689a9b32488e3075b0c423
Viktor_Ivakhnenko <ivakhnenkovd@yandex.ru>
2022-04-05 19:27:00 +0300

    Add base user models.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserModel(AbstractUser):
+    """Base user model."""
+    # Basic fields
+    first_name = models.CharField(max_length=128, verbose_name="First name")
+    last_name = models.CharField(max_length=128, verbose_name="Last name")
+
+    # Relation fields
+    settings = models.OneToOneField("users.Settings", on_delete=models.CASCADE, related_name="user_settings")
+    # TODO: create email auth
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}({self.email})"
+
+
+class Settings(models.Model):
+    """User settings model."""
+    # Basic fields
+    reminders_type = models.CharField(
+        max_length=...,
+        choices=...,
+        default=...,
+        verbose_name="Default reminders type"
+    )  # TODO: add typing after reminders create
+    photo = models.ImageField(verbose_name="User photo")
+    timezone = models.CharField(default='UTC', max_length=100, verbose_name="Timezone")
+    telegram_id = models.CharField(blank=True, null=True, verbose_name="Telegram ID")
+    # TODO: create user -> automate created user-settings
