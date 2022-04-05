68c5fbfce667f6624d318380f5c6bb8fd331bc7d
Viktor_Ivakhnenko <ivakhnenkovd@yandex.ru>
2022-04-05 19:08:09 +0300

    Create users app.

--- project_config/settings.py
+++ project_config/settings.py
@@ -34,12 +34,15 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # Django apps
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Custom apps
+    "users",
 ]
 
 MIDDLEWARE = [
