0956d2156985dc397a83933bf6d38faa45294fa6
Viktor_Ivakhnenko <ivakhnenkovd@yandex.ru>
2022-04-05 19:03:51 +0300

    Add local docker-compose (db only).

--- project_config/settings.py
+++ project_config/settings.py
@@ -28,8 +28,7 @@ try:
 except KeyError:
     DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -84,7 +83,7 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": 5432,
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
