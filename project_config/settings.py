33d6955e0b5ac6450796ddf33633794575420eaf
Viktor Ivakhnenko <ivakhnenkovd@yandex.ru>
2022-06-24 16:54:57 +0300

    fix(static) Fix all static and small fix base.html

--- project_config/settings.py
+++ project_config/settings.py
@@ -15,9 +15,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = join(BASE_DIR, ".env")
 load_dotenv(dotenv_path)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -113,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -127,11 +123,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Logging
 # LOGGING_CONFIG = None
